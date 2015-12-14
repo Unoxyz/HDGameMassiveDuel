1e47255072daa637e107aff79bc953d5f4f8f309
Namun Cho <namun.cho@gmail.com>
2015-12-15 01:43:10 +0900

    created main.js

--- core.js
+++ core.js
@@ -199,23 +199,5 @@ function tempReverse(array) {
   return array.reverse();
 }
 
-// Ajax
-var xhr = new XMLHttpRequest();
-xhr.open('POST', './data.php');
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send();
 
-var player1, player2;
-xhr.onload = function () {
-  // document.write(xhr.responseText);
-  var data = JSON.parse(xhr.responseText);
-
-  user1 = new Player(data[0].studentId, data[0].name, data[0].maxMemory, data[0].memories);
-  user2 = new Player(data[1].studentId, data[1].name, data[1].maxMemory, data[1].memories);
-  player1 = user1;
-  player2 = user2;
-
-  playGame(player1, player2);
-  console.log("Players", player1, player2);
-  console.log("gameResults", gameResults);
 };
