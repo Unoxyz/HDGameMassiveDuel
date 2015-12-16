dcf3f9a945062af55df74da1d63bf567f29ce83f
Namun Cho <namun.cho@gmail.com>
2015-12-17 00:07:57 +0900

    json --> result csv php

---
+++ json2result.php
@@ -0,0 +1,10 @@
+<?php
+$file = file_get_contents("player_result.json");
+$data = json_decode($file,true);
+foreach($data as $row){
+	$id=$row['studentId'];
+	$name=$row['name'];
+	$totalScore=array_sum($row['gameScore']);
+	echo "$id,$name,$totalScore<br>";
+}
+?>
